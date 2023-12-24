10a0bdb937562700f74b5b8e2ce3f912d3beda4d
kc++ <2745296803@qq.com>
2023-12-24 19:43:54 +0800

    first commit

---
+++ test.lua
@@ -0,0 +1,23 @@
+-- num = 100
+
+-- print (num)
+
+-- function sayhello()
+--     print ("hello")
+-- end
+
+-- sayhello()
+
+-- function max(a , b)
+--     if  a > b then
+--         return a
+--     else
+--         return b
+--     end
+-- end
+
+-- print(max(100,200))
+
+for i = 1, 10 do
+    print(i)
+end
